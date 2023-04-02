30e70ddbb32e9442a3fef8e525ac8c44137b8d86
sustapakhreen <sushta48@gmail.com>
2023-04-02 17:02:10 -0500

    Array-map-method lab

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased(){
+  return tutorials.map((tutorial) =>
+    tutorial
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ")
+);
 }
